cfc7db39e9db4c2de594c43553a2970feb93f0fc
Sujit-Pawar-9 <sujitpawar18599@gmail.com>
2023-10-16 18:00:10 +0545

    changes

--- Database/Assignments/trigger.sql
+++ Database/Assignments/trigger.sql
@@ -1,9 +1,26 @@
+
+/*
 drop trigger if exists tr1;
 delimiter $
-create trigger tr1 before delete on d for each row
-begin
-	insert into d1 values(old.c1,old.ename,old.c3,current_DATE(),current_time(),user());
+create trigger tr1 before insert on d for each row 
+BEGIN
+
+	if dayname(now())='monday' then 
+		signal sqlstate '42000' set message_text="invalid transaction bcz today is monday";
+	end if;
+end $
+delimiter ; */
+
+
+drop trigger if exists tr1;
+delimiter $
+create trigger tr1 before insert on d for each row 
+BEGIN
+    declare z int;
+	select max(c1) +1 into z from d;
+	set new.c1=z;
 end $
 delimiter ;
 
-	
+
+	 
